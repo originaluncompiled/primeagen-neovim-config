8f8f336f0298e7571757536818cf92519d20026c
Original <original@CARSTEN-PC.lan>
2026-02-10 06:01:57 +0200

    fixed it for v0.12.0-dev

--- lua/theprimeagen/lazy/treesitter.lua
+++ lua/theprimeagen/lazy/treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
+        require("nvim-treesitter.config").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
@@ -32,14 +32,7 @@ return {
             },
         })
 
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        treesitter_parser_config.templ = {
-            install_info = {
-                url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = {"src/parser.c", "src/scanner.c"},
-                branch = "master",
-            },
-        }
+        vim.treesitter.language.register('templ', 'templ')
 
         vim.treesitter.language.register("templ", "templ")
     end
